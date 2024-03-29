07f4f1e53134b4af8204392a8fed31712b50a8fa
Hardik Sachan <hardiksachan@Hardiks-MacBook-Pro.local>
2024-03-29 22:29:35 +0530

    handle 404 response

--- app/main.js
+++ app/main.js
@@ -6,7 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    const req = data.toString().split("\r\n");
+    const path = req[0].split(" ")[1].trim();
+
+    if (path === "" || path === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
   });
 
   socket.on("close", () => {
