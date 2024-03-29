79eaebcb168d20e7dbd0d8506db6e9e9b32f46c0
Hardik Sachan <hardiksachan@Hardiks-MacBook-Pro.local>
2024-03-29 22:58:53 +0530

    file resposne

--- app/main.js
+++ app/main.js
@@ -1,11 +1,12 @@
 const net = require("net");
+const fs = require("node:fs/promises");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", (data) => {
+  socket.on("data", async (data) => {
     const req = data.toString().split("\r\n");
     const path = req[0].split(" ")[1].trim();
 
@@ -13,6 +14,26 @@ const server = net.createServer((socket) => {
     for (let i = 1; i < req.length; i++) {
       const [k, v] = req[i].split(": ");
       headers[k] = v;
+    }
+
+    if (path.startsWith("/files/")) {
+      const fileName = path.substring(7);
+      const dir = process.argv[3];
+
+      try {
+        const fileData = await fs.readFile(dir + fileName, "utf-8");
+        return socket.write(
+          "HTTP/1.1 200 OK\r\n" +
+            "Content-Type: application/octet-stream\r\n" +
+            "Content-Length: " +
+            fileData.length +
+            "\r\n\r\n" +
+            fileData +
+            "\r\n"
+        );
+      } catch (_err) {
+        return socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
     }
 
     if (path.startsWith("/user-agent")) {
