123f11caed7b4429d138808fc51ef94dc098e8f7
Hardik Sachan <hardiksachan@Hardiks-MacBook-Pro.local>
2024-03-29 23:21:09 +0530

    file resposne

--- app/main.js
+++ app/main.js
@@ -1,42 +1,45 @@
 const net = require("net");
-const fs = require("node:fs/promises");
+const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("data", async (data) => {
+  socket.on("data", (data) => {
     const req = data.toString().split("\r\n");
     const path = req[0].split(" ")[1].trim();
 
-    const headers = {};
-    for (let i = 1; i < req.length; i++) {
-      const [k, v] = req[i].split(": ");
-      headers[k] = v;
-    }
-
     if (path.startsWith("/files/")) {
       const fileName = path.substring(7);
-      const dir = process.argv[3];
+      const dir = process.argv[process.argv.indexOf("--directory") + 1];
 
-      try {
-        const fileData = await fs.readFile(dir + fileName, "utf-8");
-        return socket.write(
-          "HTTP/1.1 200 OK\r\n" +
-            "Content-Type: application/octet-stream\r\n" +
-            "Content-Length: " +
-            fileData.length +
-            "\r\n\r\n" +
-            fileData +
-            "\r\n"
-        );
-      } catch (_err) {
-        return socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      if (!fs.existsSync(dir + fileName)) {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        socket.end();
+        return;
       }
+
+      const fileData = fs.readFileSync(dir + fileName);
+
+      return socket.write(
+        "HTTP/1.1 200 OK\r\n" +
+          "Content-Type: application/octet-stream\r\n" +
+          "Content-Length: " +
+          fileData.length +
+          "\r\n\r\n" +
+          fileData +
+          "\r\n"
+      );
     }
 
     if (path.startsWith("/user-agent")) {
+      const headers = {};
+      for (let i = 1; i < req.length; i++) {
+        const [k, v] = req[i].split(": ");
+        headers[k] = v;
+      }
+
       const userAgent = headers["User-Agent"];
       return socket.write(
         "HTTP/1.1 200 OK\r\n" +
@@ -63,11 +66,11 @@ const server = net.createServer((socket) => {
       );
     }
 
-    if (path === "" || path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    if (path === "/") {
+      return socket.write("HTTP/1.1 200 OK\r\n\r\n");
     }
+
+    return socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
   });
 
   socket.on("close", () => {
